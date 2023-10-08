b2b0c31f71d35d723afa61325acc89287acc9ea2
Amjad <amjadshakir@gmail.com>
2023-10-08 21:08:36 +0100

    fix: logic errors in exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,39 @@
+function isFloat(n){
+	return Number(n) === n && n % 1 !== 0;
+}
+
 export function getFillings(sandwich) {
 	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+	return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+		return person.city === "Manchester";	
 }
 
 export function getBusNumbers(people) {
 	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+	let busNumbers = people/40;
+	if (isFloat(busNumbers)){
+		return Number(Math.trunc(busNumbers)) + 1;	
+	} else return busNumbers;		
 }
 
 export function countSheep(arr) {
 	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+	let count = 0;
+	arr.forEach(element => {
+		if(element==='sheep'.toLowerCase()){
+			count = count + 1;
+		}
+	})
+	return count;
 }
 
 export function hasMPostCode(person) {
 	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	if (person.address.city ==="Manchester"){
+		return true;
+	} else return false;
 }
